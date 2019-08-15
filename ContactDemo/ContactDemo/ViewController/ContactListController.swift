e7bec54bc52f8225ac7b0940462cdcf3356c0f64
Shivam Saxena <shivambissario@gmail.com>
2019-08-15 17:25:45 +0530

    #Project hierarchy restructured and custom fonts and resources added

---
+++ ContactDemo/ContactDemo/ViewController/ContactListController.swift
@@ -0,0 +1,36 @@
+//
+//  ContactListController.swift
+//  ContactDemo
+//
+//  Created by apple on 8/15/19.
+//  Copyright © 2019 Shivam Saxena. All rights reserved.
+//
+
+import UIKit
+
+class ContactListController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonClicked))
+       // navigationItem.tit
+        navigationItem.title = ""
+    }
+
+    @objc func addButtonClicked() {
+       print("Add button Clicked")
+    }
+    
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+}
